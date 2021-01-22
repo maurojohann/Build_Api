e157954b14c708f83b7d54981b20f6adaaca3535
Mauro Cezar Johann <mauro.johann@gmail.com>
2021-01-22 14:43:18 -0300

    Create liste of the Years

--- api/server.js
+++ api/server.js
@@ -6,17 +6,19 @@ app.get('/:name', async (req, res) => {
 
     const response = await axios('https://jsonmock.hackerrank.com/api/movies/search/?Title=' + req.params.name)
     var fils = []
-
+    var current
     for (let index = response.data["page"]; index <= response.data['total_pages']; index++) {
 
         await axios('https://jsonmock.hackerrank.com/api/movies/search/?Title=' + req.params.name +'&page=' + index).then(function (respon){
-           fils.push(respon.data['data'])
+          //fils.push(respon.data.data.map(m => m.concat(m.Year)))
+          respon.data.data.map(m => fils.push(m.Year))
+          
         })
-        console.log(fils.length)
-
+        
     }
+  
     console.log(fils)
-    return res.json(response.data)
+    return res.json(fils)
 
 })
 app.listen('55555')
