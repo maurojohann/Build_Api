36e4e13041934fa2841731b27ac82b4b3a646147
Mauro Cezar Johann <mauro.johann@gmail.com>
2021-01-22 21:11:43 -0300

    group films by years

--- api/server.js
+++ api/server.js
@@ -5,20 +5,34 @@ const axios = require('axios').default
 app.get('/:name', async (req, res) => {
 
     const response = await axios('https://jsonmock.hackerrank.com/api/movies/search/?Title=' + req.params.name)
-    var fils = []
+    var films = []
     var current
     for (let index = response.data["page"]; index <= response.data['total_pages']; index++) {
 
         await axios('https://jsonmock.hackerrank.com/api/movies/search/?Title=' + req.params.name +'&page=' + index).then(function (respon){
-          //fils.push(respon.data.data.map(m => m.concat(m.Year)))
-          respon.data.data.map(m => fils.push(m.Year))
-          
+          //films.push(respon.data.data.map(m => m.concat(m.Year)))
+          respon.data.data.map(m => films.push(m.Year))
+                 
         })
         
     }
-  
-    console.log(fils)
-    return res.json(fils)
+
+    
+    groupByYears = films.reduce((groups, item) => {
+      const group = (groups[item] || [])
+      group.push(item)
+      console.log('groups[item] ' + groups[item])
+      console.log('group ' + group)
+      groups[item] = group
+      console.log('groups[item] 2 ' + groups[item])
+      console.log('groups ' + groups)
+      return groups
+    }, {})
+    
+      console.log(groupByYears)
+      
+        
+    return res.json(groupByYears)
 
 })
 app.listen('55555')
